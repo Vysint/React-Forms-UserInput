366e29f82699fffc256bf89e2b01f4f1e7ff966c
Vincent Kipngetich <vinniebluz@icloud.com>
2022-09-16 12:56:25 +0300

    Handled form submissions and getting user input

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,29 @@
+import { useRef, useState } from "react";
+
 const SimpleInput = (props) => {
+  const nameInputRef = useRef();
+  const [enteredName, setEnteredName] = useState("");
+
+  const nameInputChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+
+    console.log(enteredName);
+  };
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSubmissionHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          type="text"
+          id="name"
+          onChange={nameInputChangeHandler}
+          ref={nameInputRef}
+          value={enteredName}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
